17a3882176720150a52c6e46a32a474396783d49
Sofi-240 <54475550+Sofi-240@users.noreply.github.com>
2023-09-04 13:59:32 +0300

    debugging

--- cv_models/utils.py
+++ cv_models/utils.py
@@ -359,7 +359,7 @@ def compute_hessian_3D(
 
     X = tf.expand_dims(X, axis=-1)
 
-    dFww = tf.nn.convolution(X, kww, padding='VALID') * 0.25
+    dFww = tf.nn.convolution(X, kww, padding='VALID')
 
     dFws = tf.nn.convolution(X, kws, padding='VALID') * 0.25
 
@@ -375,54 +375,3 @@ def compute_hessian_3D(
     return hessian_mat
 
 
-if __name__ == '__main__':
-    case1 = tf.ones(shape=(1, 3, 3, 3), dtype=tf.float32)
-
-    cords_case1 = tf.where(tf.equal(case1, 1.0))
-    neighbor1 = make_neighborhood3D(init_cords=cords_case1, con=3, origin_shape=(1, 3, 3, 3))
-    assert neighbor1.shape[0] == 1
-
-    case2 = np.array([
-        [
-            [0.00, 0.01, 0.02, 0.03, 0.04],
-            [0.05, 0.06, 0.07, 0.08, 0.09],
-            [0.10, 0.11, 0.12, 0.13, 0.14],
-            [0.15, 0.16, 0.17, 0.18, 0.19],
-            [0.20, 0.21, 0.22, 0.23, 0.24]
-        ],
-        [
-            [0.25, 0.26, 0.27, 0.28, 0.29],
-            [0.30, 0.31, 0.32, 0.33, 0.34],
-            [0.35, 0.36, 0.37, 0.38, 0.39],
-            [0.40, 0.41, 0.42, 0.43, 0.44],
-            [0.45, 0.46, 0.47, 0.48, 0.49]
-        ],
-        [
-            [0.50, 0.51, 0.52, 0.53, 0.54],
-            [0.55, 0.56, 0.57, 0.58, 0.59],
-            [0.60, 0.61, 0.62, 0.63, 0.64],
-            [0.65, 0.66, 0.67, 0.68, 0.69],
-            [0.70, 0.71, 0.72, 0.73, 0.74]
-        ],
-        [
-            [0.75, 0.76, 0.77, 0.78, 0.79],
-            [0.80, 0.81, 0.82, 0.83, 0.84],
-            [0.85, 0.86, 0.87, 0.88, 0.89],
-            [0.90, 0.91, 0.92, 0.93, 0.94],
-            [0.95, 0.96, 0.97, 0.98, 0.99]
-        ],
-        [
-            [1.00, 1.01, 1.02, 1.03, 1.04],
-            [1.05, 1.06, 1.07, 1.08, 1.09],
-            [1.10, 1.11, 1.12, 1.13, 1.14],
-            [1.15, 1.16, 1.17, 1.18, 1.19],
-            [1.20, 1.21, 1.22, 1.23, 1.24]
-        ]
-    ])
-    case2 = tf.expand_dims(case2, axis=0)
-    case2 = tf.cast(case2, dtype=tf.float32)
-
-    g_xyz = compute_central_gradient3D(case2)
-    g_xyz = tf.reduce_mean(tf.reshape(g_xyz, shape=(-1, 3)), axis=0)
-
-    g_xy = compute_central_gradient2D(case2)
